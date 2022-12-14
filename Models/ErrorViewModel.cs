8bc63ae639fcd446bafa960869fc8a8639e81eba
Olt Gashi <oltgvshi@gmail.com>
2022-12-14 18:39:05 +0100

    files

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProfileMatching.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
