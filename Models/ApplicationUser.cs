776179e0cb5908f47aa2ee4d75ac6e7b80d7ca36
yllkaselimi <ys51425@ubt-uni.com>
2023-01-06 16:40:23 +0100

    Login and Register

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ProfileMatching.Models;
+
+namespace ASP.NETCoreIdentityCustom.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+
+}
