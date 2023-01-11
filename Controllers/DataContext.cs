a591453ca41cf981580aec82403604f9d24c24d1
Olt Gashi <68893935+oltgvshi@users.noreply.github.com>
2023-01-11 14:30:42 +0100

    ratings crud

--- Controllers/DataContext.cs
+++ Controllers/DataContext.cs
@@ -25,6 +25,8 @@ namespace ProfileMatching.Controllers
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<ClientDetail> ClientDetails { get; set; }
 
+        public DbSet<Rating> Ratings { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
