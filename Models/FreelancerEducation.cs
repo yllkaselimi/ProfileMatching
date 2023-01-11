a48b5b7af7da493c443eb08ffcc8695748b4b65f
yllkaselimi <ys51425@ubt-uni.com>
2023-01-11 13:12:13 +0100

    FreelancerEducation CRUD

---
+++ Models/FreelancerEducation.cs
@@ -0,0 +1,23 @@
+﻿using ASP.NETCoreIdentityCustom.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProfileMatching.Models
+{
+    public class FreelancerEducation
+    {
+        [Key]
+        public int FreelancerEducationid { get; set; }
+        public string InstituteName { get; set; }
+        public string Degree { get; set; }
+        public string FieldOfStudy { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+    }
+}
