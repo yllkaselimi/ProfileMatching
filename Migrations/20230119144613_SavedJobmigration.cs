7286e64e5217fe6db1322df7849a0e36aa5a2801
Era Ismaili <eraaismaili@gmail.com>
2023-01-19 16:20:43 +0100

    SavedJobs

---
+++ Migrations/20230119144613_SavedJobmigration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfileMatching.Migrations
+{
+    public partial class SavedJobmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SavedJobs",
+                columns: table => new
+                {
+                    SavedJobsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    JobPostId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedJobs", x => x.SavedJobsId);
+                    table.ForeignKey(
+                        name: "FK_SavedJobs_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_SavedJobs_JobPosts_JobPostId",
+                        column: x => x.JobPostId,
+                        principalTable: "JobPosts",
+                        principalColumn: "JobPostId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedJobs_JobPostId",
+                table: "SavedJobs",
+                column: "JobPostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedJobs_UserId",
+                table: "SavedJobs",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavedJobs");
+        }
+    }
+}
