abc57a8d6537324095edbcebf85ca070744bdfac
Tringa Desku <tringadesku@gmail..com>
2023-01-22 20:04:32 +0100

    Filter-And-Search-Freelancers

--- Controllers/FreelancersController.cs
+++ Controllers/FreelancersController.cs
@@ -13,18 +13,24 @@ namespace ProfileMatching.Controllers
 
         private readonly DataContext _context;
 
+
         public FreelancersController(DataContext context)
         {
             _context = context;
         }
+
         public async Task<IActionResult> Index()
         {
+            ViewData["Categories"] = _context.Categories.ToList();
+
             var dataContext = _context.FreelancerDetails.Include(f => f.ApplicationUser).Include(f => f.Category).Include(f => f.City);
             return View(await dataContext.ToListAsync());
         }
 
         public async Task<IActionResult> Suggested(int? id)
         {
+            ViewData["Categories"] = _context.Categories.ToList();
+
             var freelancers = _context.FreelancerDetails
                 .Include(f => f.ApplicationUser)
                 .Include(f => f.Category).Include(f => f.City)
@@ -35,6 +41,40 @@ namespace ProfileMatching.Controllers
                 return NotFound();
             }
             return View(await freelancers.ToListAsync());
+        }
+
+        public async Task<IActionResult> Filtered(int? id)
+        {
+            ViewData["Categories"] = _context.Categories.ToList();
+
+            var category = _context.Categories.Where(x => x.CategoryId == id).First();
+            ViewData["FilteredCategoryName"] = category.CategoryName;
+
+            var freelancers = _context.FreelancerDetails
+                .Include(f => f.ApplicationUser)
+                .Include(f => f.Category).Include(f => f.City)
+                .Where(f => f.Category.CategoryId == id);
+
+            if (freelancers == null)
+            {
+                return NotFound();
+            }
+            return View(await freelancers.ToListAsync());
+        }
+
+        public async Task<IActionResult> SearchFreelancers(string teksti)
+        {
+            ViewData["Categories"] = _context.Categories.ToList();
+
+            ViewData["SearchInput"] = teksti;
+
+            var dataContext = _context.FreelancerDetails.Include(f => f.ApplicationUser).Include(f => f.Category).Include(f => f.City);
+            var result = dataContext.Where(f => f.ApplicationUser.UserName.ToLower().Contains(teksti.ToLower())
+                || f.ApplicationUser.LastName.ToLower().Contains(teksti.ToLower())
+                || f.City.CityName.ToLower().Contains(teksti.ToLower())
+                || f.Category.CategoryName.ToLower().Contains(teksti.ToLower()));
+
+            return View(await result.ToListAsync());
         }
     }
 }
