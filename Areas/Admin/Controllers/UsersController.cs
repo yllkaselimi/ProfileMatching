f2149b68a77dc441b5e04429a45ff0888da6c0e1
Tringa Desku <tringadesku@gmail..com>
2023-02-08 03:28:27 +0100

    Update on admin area

---
+++ Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProfileMatching.Controllers;
+using ASP.NETCoreIdentityCustom.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace ProfileMatching.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Route("admin/[controller]/[action=Index]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IUserStore<IdentityUser> _userStore;
+        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(DataContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+
+        }
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+       
+    }
+}
