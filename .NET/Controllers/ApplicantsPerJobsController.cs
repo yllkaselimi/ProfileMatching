ab62f5912150c87d57bb0f798f7a36164c22ad5b
Era Ismaili <eraaismaili@gmail.com>
2023-06-04 20:23:21 +0200

    Update ApplicantsPerJobsController

--- .NET/Controllers/ApplicantsPerJobsController.cs
+++ .NET/Controllers/ApplicantsPerJobsController.cs
@@ -25,10 +25,10 @@ namespace ProfileMatching.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ApplicantsPerJob>>> GetApplicantsPerJobs()
         {
-          if (_context.ApplicantsPerJobs == null)
-          {
-              return NotFound();
-          }
+            if (_context.ApplicantsPerJobs == null)
+            {
+                return NotFound();
+            }
             return await _context.ApplicantsPerJobs.ToListAsync();
         }
 
@@ -36,10 +36,10 @@ namespace ProfileMatching.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<ApplicantsPerJob>> GetApplicantsPerJob(int id)
         {
-          if (_context.ApplicantsPerJobs == null)
-          {
-              return NotFound();
-          }
+            if (_context.ApplicantsPerJobs == null)
+            {
+                return NotFound();
+            }
             var applicantsPerJob = await _context.ApplicantsPerJobs.FindAsync(id);
 
             if (applicantsPerJob == null)
@@ -50,22 +50,71 @@ namespace ProfileMatching.Controllers
             return applicantsPerJob;
         }
 
-        // GET: api/ApplicantsPerJobs/GetJobpostsByClientId/5
-        [HttpGet("GetJobpostsByClientId/{clientId}")]
-        public async Task<ActionResult<IEnumerable<JobPost>>> GetJobpostsByClientId(string clientId)
+
+        // i kthen jobpostat qe i ka kriju klienti i cili eshte logged in
+        // api/ApplicantsPerJobs/GetJobPostsByClientId
+        [HttpGet("GetJobpostsByClientId")]
+        public async Task<ActionResult<IEnumerable<JobPost>>> GetJobPostsByClientId()
         {
-            var jobPosts = await _context.JobPosts
-                .Where(j => j.UserId == clientId)
+            // Get the user credentials for the first record
+            var userCredentials = await _context.UserCredentials.FirstOrDefaultAsync();
+
+            // Check if userCredentials exist and the user role is "Client"
+            if (userCredentials != null && userCredentials.UserRole == "Client")
+            {
+                var jobPosts = await _context.JobPosts
+                    .Where(j => j.UserId == userCredentials.UserId)
+                    .ToListAsync();
+
+                if (jobPosts.Count == 0)
+                {
+                    return NotFound("No job posts found for the client");
+                }
+
+                return jobPosts;
+            }
+
+            return NotFound("User not logged in!");
+        }
+
+        /* metoda qe i kthen punet ku ti je hired at, si freelancer
+        */
+        // GET: api/ApplicantsPerJobs/GetMyHiredJobs
+        [HttpGet("GetMyHiredJobs")]
+        public async Task<ActionResult<IEnumerable<JobPost>>> GetMyHiredJobs()
+        {
+            // Get the user credentials for the first record
+            var userCredentials = await _context.UserCredentials.FirstOrDefaultAsync();
+
+
+            // Check if the logged-in user is a "Freelancer"
+            if (userCredentials == null || userCredentials.UserRole != "Freelancer")
+            {
+                return BadRequest("Only freelancers can access this resource");
+            }
+
+            var hiredJobPostIds = await _context.ApplicantsPerJobs
+                .Where(a => a.UserId == userCredentials.UserId && a.HiredStatus == true)
+                .Select(a => a.JobPostId)
                 .ToListAsync();
 
-            if (jobPosts == null)
+            var jobPosts = await _context.JobPosts
+                .Where(j => hiredJobPostIds.Contains(j.JobPostId))
+                .ToListAsync();
+
+            if (jobPosts.Count == 0)
             {
-                return NotFound();
+                return NotFound("No hired job posts found for the freelancer");
             }
 
             return jobPosts;
         }
 
+
+        /* metoda qe i kthen user data t'aplikanteve qe jane hired ne qat pune
+           qikjo mundet me vyjt veq si liste psh nese e bojm ni section t'hired applicants kush osht n'qat pune 
+           se perndryshe kur bohesh log in si freelancer, duhet mu kon ni metod tjeter qe ti thirr punt ku je hired
+        */
         // GET: api/ApplicantsPerJobs/GetHiredApplicantsForJobPost/5
         [HttpGet("GetHiredApplicantsForJobPost/{jobPostId}")]
         public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetHiredApplicantsForJobPost(int jobPostId)
@@ -73,7 +122,7 @@ namespace ProfileMatching.Controllers
             var hiredApplicants = await _context.ApplicantsPerJobs
                 .Where(a => a.JobPostId == jobPostId)
                 .Where(a => a.HiredStatus == true)
-                .Select(a => a.ApplicationUser) 
+                .Select(a => a.ApplicationUser)
                 .ToListAsync();
 
             if (hiredApplicants == null)
@@ -120,10 +169,10 @@ namespace ProfileMatching.Controllers
         [HttpPost]
         public async Task<ActionResult<ApplicantsPerJob>> PostApplicantsPerJob(ApplicantsPerJob applicantsPerJob)
         {
-          if (_context.ApplicantsPerJobs == null)
-          {
-              return Problem("Entity set 'DataContext.ApplicantsPerJobs'  is null.");
-          }
+            if (_context.ApplicantsPerJobs == null)
+            {
+                return Problem("Entity set 'DataContext.ApplicantsPerJobs'  is null.");
+            }
             _context.ApplicantsPerJobs.Add(applicantsPerJob);
             await _context.SaveChangesAsync();
 
